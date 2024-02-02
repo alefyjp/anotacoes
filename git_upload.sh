47cfdaa9ea92070af71d0d8ad4b1b7007c18268f
Alefy <alefyjp@gmail.com>
2024-02-02 17:39:44 -0300

    sex 02 fev 2024 17:39:44 -03

--- git_upload.sh
+++ git_upload.sh
@@ -6,12 +6,17 @@ DATAHORA=$(date)
 
 
 # ===== Enviar dados ===== #
-
+echo "=="
+echo "Arquivos que serao enviados"
 git add -A
 git commit -m "$DATAHORA"
 
+echo "=="
 echo "Utilize o token: "
 echo ""
 cat $ARQUIVO_TOKEN
 echo""
+
+echo "=="
+echo "Realizando envio dos arquivos"
 git push
