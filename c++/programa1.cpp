5d1516584e61ba30cc677f3256c1acdb1142a8dc
root <root@VSE-TI-02.visual_ad.srv>
2024-06-28 18:12:16 -0300

    Fri Jun 28 18:12:16 -03 2024

---
+++ c++/programa1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+int main(){
+	//Variaveis
+	string destino, origem;
+	destino="/mnt/bkp";
+
+	//Testando saidas
+	cout <<destino;
+	getchar();
+	return 0;
+}
