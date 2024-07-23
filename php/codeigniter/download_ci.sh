03b03455d61225775d8002ebe658cfd9289a80cf
root <root@VSE-TI-02.visual_ad.srv>
2024-07-23 18:10:16 -0300

    Tue Jul 23 18:10:15 -03 2024

---
+++ php/codeigniter/download_ci.sh
@@ -0,0 +1,10 @@
+#"/bin/bash
+wget https://api.github.com/repos/codeigniter4/framework/zipball/v4.5.3
+
+#Descompactar
+unzip v4.5.3
+
+rm v4.5.3
+
+# Renomear pasta
+mv codeigniter4-framework-be27314/ ci
